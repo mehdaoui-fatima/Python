af2a1259c250e275c04e49af46910b572cc9ee32
Fatima ezzahra Mehdaoui <fmehdaou@e3r4p11.1337.ma>
2025-06-29 16:48:11 +0100

    add: ex01 day00

--- day00/ex01/format_ft_time.py
+++ day00/ex01/format_ft_time.py
@@ -1,8 +1,10 @@
-from time import time, strftime, gmtime
+import time
 
-epoch = time()
-epoch_struct= gmtime()
+epoch = time.time()
+# epoch_struct= time.gmtime()
+local_time = time.localtime()
 
 #string fornatting 
-print(f"Seconds since January 1, 1970: {epoch:,.4f} or {epoch:.3} in scientific notation")
-print(strftime("%b %d %Y", epoch_struct))
+print(f"Seconds since January 1, 1970: {epoch:,.4f} or {epoch:.2e} in scientific notation")
+# print(time.strftime("%b %d %Y", epoch_struct))
+print(time.strftime("%b %d %Y", local_time))
