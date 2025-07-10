9f77ce7cf0a532395e6a6709023bcd0c9bf5209a
Fatima ezzahra Mehdaoui <fmehdaou@e3r8p13.1337.ma>
2025-07-10 08:26:34 +0100

    add exercises

---
+++ day00/ex04/whatis.py
@@ -0,0 +1,24 @@
+import sys
+
+
+
+# if the function def doest contain return statement, None is.
+def parity(x: int) -> str:
+
+    if (int(x) % 2 == 0):
+        return "I'm Even."
+    else:
+        return "I'm Odd."
+
+
+try:
+    #assert condition, error_message  
+    assert len(sys.argv) == 2, "more than one argument is provided"
+    int(sys.argv[1])
+    print (parity(sys.argv[1]))
+
+except ValueError as e:
+    print(f'{AssertionError.__name__}: argument is not an integer')
+
+except Exception as e:
+    print(f'{AssertionError.__name__}: {e}')
