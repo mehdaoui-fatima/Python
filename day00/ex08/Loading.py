ac658862440a2fadc13005c66098f94929ccfaed
Fatima ezzahra Mehdaoui <fmehdaou@e3r2p16.1337.ma>
2025-07-23 21:43:11 +0100

    add exercise 08 yield operator

---
+++ day00/ex08/Loading.py
@@ -0,0 +1,22 @@
+
+
+
+# TODO
+# doc string
+# norm and clear code from tests files
+# tests with file correction
+#  understanding well  
+
+
+def ft_tqdm(lst: range) -> None:
+    total = len(lst)
+    bar_width = 70
+
+    for i, elem in enumerate(lst):
+        percent = int((i + 1) / total * 100)
+        filled = int((i + 1) / total * bar_width)
+        bar = '=' * (filled - 1) + '>'
+       
+
+        print(f"{percent:3d}%|[{bar:<{bar_width}}]| {i + 1}/{total}", end='\r')
+        yield elem
