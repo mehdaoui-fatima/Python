bc1181c9e60f8ee603acb3c2af3a0cdd30236249
Fatima ezzahra Mehdaoui <fmehdaou@e3r7p12.1337.ma>
2025-08-03 19:42:23 +0100

    add load_image

--- day01/ex02/load_image.py
+++ day01/ex02/load_image.py
@@ -1,16 +1,17 @@
 import numpy as np
 from PIL import Image
 
+
 def ft_load(path: str) -> np.array:
+    """loads an image, prints its format and pixels content in RGB format."""
 
     try:
-        assert len(path) > 0, 'Invalid path file'
         image = Image.open(path)
-        assert image.format in ['JPEG', 'JPG'], "Invalid file format"
+        assert image.format in ['JPEG', 'JPG'], "Only JPEG/JPG are supported"
         np_image = np.array(image)
-      
+
+        print(f"The shape of image is: {np_image.shape}")
         return np_image
 
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
-
